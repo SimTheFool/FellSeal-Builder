b5012901dc79e22396e5c58139ee9ea9f2ec3bf9
SimTheFool <simon.sauzede@gmail.com>
2022-10-27 22:10:13 +0200

    feat: wip

--- builder/cli/build/index.js
+++ builder/cli/build/index.js
@@ -3,6 +3,8 @@ import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import getTsConfig from "./getTsConfig.js";
 import getEntryPoints from "./getEntryPoints.js";
+import path from "path";
+import fs from "fs/promises";
 
 const args = yargs(hideBin(process.argv))
   .option("tsconfig", {
@@ -12,19 +14,54 @@ const args = yargs(hideBin(process.argv))
   })
   .parse();
 
-const getBuildConfig = async () => {
-  const { compilerOptions, include } = await getTsConfig(args.tsconfig);
+const tsConfigAliases = (aliases) => {
+  const changeAliases = Object.entries(aliases).map(
+    ([alias, dest]) =>
+      (build) => {
+        const regex = new RegExp(alias.replace(/\*+/, "(.*)"));
+        build.onResolve({ filter: regex }, changeAlias(regex, dest));
+      }
+  );
 
+  const changeAlias = (regex, dest) => (args) => {
+    const endpoint = args.path.match(regex)[1];
+    const newPath = dest[0].replace("*", endpoint);
+    console.log("***", newPath);
+    args.path = newPath;
+  };
+
+  return {
+    name: "tsConfigAliases",
+    setup: (build) => {
+      //changeAliases.forEach((change) => change(build));
+      //build.onResolve({ filter: /.*/ }, async (args) => {
+      //  console.log("resolve", args);
+      //});
+      build.onLoad({ filter: /.*/, namespace: "file" }, async (args) => {
+        console.log("onload", args);
+      });
+    },
+  };
+};
+
+const getBuildConfig = async () => {
+  const {
+    compilerOptions: { outDir, paths },
+    include,
+  } = await getTsConfig(args.tsconfig);
+
+  console.log("aaa", await getEntryPoints(include));
   return {
     tsconfig: args.tsconfig,
     entryPoints: await getEntryPoints(include),
-    outdir: compilerOptions.outDir,
+    outdir: outDir,
     outbase: "./src",
     bundle: true,
     platform: "node",
     banner: {
       js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);",
     },
+    plugins: [tsConfigAliases(paths)],
   };
 };
 
