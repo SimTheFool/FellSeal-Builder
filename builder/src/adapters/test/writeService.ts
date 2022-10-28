35692da70db869cec7efa67556825b0fd0a28bd6
SimTheFool <simon.sauzede@gmail.com>
2022-10-28 10:19:06 +0200

    feat: wip

--- builder/src/adapters/test/writeService.ts
+++ builder/src/adapters/test/writeService.ts
@@ -1,6 +1,6 @@
 import { Character } from "@domain/character/Character.js";
 import { newAppResult } from "@utils/result/Result.js";
-import { characters as fakeCharacters } from "../../../fixtures/characters.js";
+import { characters as fakeCharacters } from "@fixtures/characters.js";
 import { WriteService } from "../writeService.js";
 
 const charactersDB = fakeCharacters.map(
