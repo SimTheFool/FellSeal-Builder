6f98d6ba53c749376625b88103f437a57817da71
SimTheFool <simon.sauzede@gmail.com>
2022-12-02 18:12:12 +0100

    feat: migration with compile time assets

--- builder/src/adapters/test/writeService.ts
+++ builder/src/adapters/test/writeService.ts
@@ -1,20 +1,19 @@
 import { Character } from "@domain/character/Character.js";
-import { Job, JobType, newJob } from "@domain/Job.js";
+import { Job } from "@domain/Job.js";
 import {
   ActiveSkill,
   CounterSkill,
-  newActiveSkill,
-  newCounterSkill,
-  newPassiveSkill,
   PassiveSkill,
+  Skill,
 } from "@domain/Skill.js";
 import { characters, characters as fakeCharacters } from "@fixtures/characters";
 import { testDb } from "@utils/infra/testDb.js";
 import { newAppResult } from "@utils/result/Result.js";
-import { XMLParser } from "fast-xml-parser";
-import { keyBy } from "lodash";
-import { WriteService } from "../writeService.js";
 import { v4 as uuid } from "uuid";
+import jobsGameData from "../../assets/gameData/jobs.gdata";
+import skillsGameData from "../../assets/gameData/skills.gdata";
+import translation_skills_fr from "../../assets/translations/fr/skills.tra";
+import { WriteService } from "../writeService.js";
 
 export const newWriteService = (): WriteService => {
   migrate();
@@ -62,94 +61,22 @@ const migrate = () => {
     ...c,
   }));
 
-  /* testDb.jobs = importJobAndSkills(xmlJobs, xmlSkills); */
+  const skillsByHash = skillsGameData.reduce((acc, skill) => {
+    const { hash } = skill;
+    return {
+      ...acc,
+      [hash]: skill,
+    };
+  }, {} as Record<Skill["hash"], Skill>);
 
-  testDb.translations = {
-    fr: {
-      /* ...importTranslations(txtSkillsTranslation),
-      ...importTranslations(txtMonstersTranslation), */
-    },
-    en: {},
-  };
-};
-
-/** ############################ */
-
-const importTranslations = (
-  txtTranslations: string
-): Record<string, string> => {
-  const translationString = Buffer.from(txtTranslations, "base64").toString();
-  const translationJsonString = translationString
-    .replace(/\t/g, "") // Remove tab
-    .replace(/([\r\s\n]+)$/g, "") // Delete final empty lines
-    .replace(/"/g, '\\"') // Escape double quote
-    .replace(/{([A-Za-z0-9-]*)}/g, "$t($1)")
-    .replace(/[;\s]*([A-Za-z0-9-]+)[;\s]*=((?!").+)/gm, '\n"$1": "$2",') // Parse to json string
-    .replace(/([A-Za-z]+-[A-Za-z0-9-]*)/g, (x) => x.toLowerCase()) // replace uppercased keys
-    .slice(0, -1); // Delete last coma
-
-  //fs.writeFileSync("./test.txt", translationJsonString);
-
-  const translation = JSON.parse(["{", translationJsonString, "}"].join(""));
-  //console.log(translation);
-  return translation;
-};
-
-const importJobAndSkills = (xmlJobs: string, xmlSkills: string): Job[] => {
-  const xmlParser = new XMLParser({
-    ignoreAttributes: false,
-    attributeNamePrefix: "",
-    attributesGroupName: "",
-    ignoreDeclaration: true,
-    parseTagValue: true,
-    parseAttributeValue: true,
-  });
-
-  const parseXml = (xmlBase64: string) =>
-    xmlParser.parse(Buffer.from(xmlBase64, "base64").toString());
-
-  const parsedSkills = Object.values(parseXml(xmlSkills).XMLAbilities).flatMap(
-    ({ Ability }: any) => Ability
-  );
-  const parsedSkillsByHashname = keyBy(parsedSkills, "HashName");
-
-  const jobs: Job[] = parseXml(xmlJobs).XMLJobs.jobs.Job.map((xmlJob: any) => {
-    const skillIds: string[] = xmlJob?.learnables
-      ? xmlJob?.learnables.Tier.flatMap((t: any) => t.SkillTile).flatMap(
-          (t: any) => t.AbilityHash
-        )
-      : [];
-
-    const { actives, passives, counters } = skillIds.reduce(
-      ({ actives, passives, counters }, id) => {
-        const { HashName, Name, SpellType } = parsedSkillsByHashname[id];
-        const skillHash: string = HashName.toLowerCase();
-        const skillDescription = `${skillHash}-desc`;
-        const match = id.match(/^.*([A | P | C])\d+$/);
-        const skillType = match?.[1] || "N/A";
-
-        if (!["A", "P", "C"].includes(skillType))
-          throw new Error(`unable to parse skill ${id}`);
-
+  const jobs: Job[] = jobsGameData.map((job) => {
+    const { actives, passives, counters } = job.skills.reduce(
+      (acc, hash) => {
+        const skill = skillsByHash[hash];
+        const skillTypeLabel = `${skill.type}s` as `${Skill["type"]}s`;
         return {
-          actives: [
-            ...actives,
-            ...(skillType === "A"
-              ? [newActiveSkill(skillHash, Name, skillDescription, SpellType)]
-              : []),
-          ],
-          passives: [
-            ...passives,
-            ...(skillType === "P"
-              ? [newPassiveSkill(skillHash, Name, skillDescription)]
-              : []),
-          ],
-          counters: [
-            ...counters,
-            ...(skillType === "C"
-              ? [newCounterSkill(skillHash, Name, skillDescription)]
-              : []),
-          ],
+          ...acc,
+          [skillTypeLabel]: [...acc[skillTypeLabel], skill],
         };
       },
       { actives: [], passives: [], counters: [] } as {
@@ -159,20 +86,20 @@ const importJobAndSkills = (xmlJobs: string, xmlSkills: string): Job[] => {
       }
     );
 
-    const jobType: JobType =
-      xmlJob.PlayerJob === false
-        ? "monster"
-        : xmlJob.ClassName.match(/^.*-99$/)
-        ? "bzil"
-        : xmlJob.noVicariousGiven === true && xmlJob.OnlyForNonStory === true
-        ? "badge"
-        : xmlJob.noVicariousGiven === true
-        ? "story"
-        : "character";
-
-    const hash = xmlJob.ClassName.toLowerCase();
-    return newJob(hash, actives, passives, counters, jobType);
+    return {
+      ...job,
+      actives,
+      passives,
+      counters,
+    };
   });
 
-  return jobs;
+  testDb.jobs = jobs;
+
+  testDb.translations = {
+    fr: {
+      ...translation_skills_fr,
+    },
+    en: {},
+  };
 };
